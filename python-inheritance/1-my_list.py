44c161a3473c93052b04a66c381d62e1a23a9333
Reine-Ella <r.dusenayo@alustudent.com>
2025-04-03 20:19:38 +0200

    py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,11 +4,11 @@
 
 
 class MyList(list):
-        ''' Represents a MyList
-            '''
+    ''' Represents a MyList
+    '''
 
-                def print_sorted(self):
-                            '''
-                                    prints the list, but sorted
-                                            '''
-                                                    print(sorted(self))
+    def print_sorted(self):
+        '''
+        prints the list, but sorted
+        '''
+        print(sorted(self))
