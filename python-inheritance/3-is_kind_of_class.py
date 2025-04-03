e6d0ad403895499cd5e78be69d6131b5829a7738
Reine-Ella <r.dusenayo@alustudent.com>
2025-04-03 20:21:44 +0200

    py

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,9 +4,9 @@
 
 
 def is_kind_of_class(obj, a_class):
-        '''function: is_kind_of_class
-            obj: an object
-                a_class: a class
-                    Returns: Bool
-                        '''
-                            return isinstance(obj, a_class)
+    '''function: is_kind_of_class
+    obj: an object
+    a_class: a class
+    Returns: Bool
+    '''
+    return isinstance(obj, a_class)
